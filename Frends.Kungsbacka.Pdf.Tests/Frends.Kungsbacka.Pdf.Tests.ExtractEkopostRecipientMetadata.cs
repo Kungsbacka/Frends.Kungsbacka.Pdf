d0d620b1a3ebb7af1ea999724ef8ebebcce466d9
patriknilssonk <97089559+patriknilssonk@users.noreply.github.com>
2025-04-11 08:33:59 +0200

    Tests for PDF-extration-methods

---
+++ Frends.Kungsbacka.Pdf.Tests/Frends.Kungsbacka.Pdf.Tests.ExtractEkopostRecipientMetadata.cs
@@ -0,0 +1,88 @@
+﻿using NUnit.Framework;
+using System.Linq;
+namespace Frends.Kungsbacka.Pdf.Tests
+{
+	public class ExtractEkopostRecipientMetadataTests
+	{
+		[Test]
+		public void ExtractEkopostRecipientMetadataAndDocumentByRegex_NoMatches_ReturnsNull()
+		{
+			// Arrange
+			var pdfBytes = TestHelper.GetTestDocument(TestHelper.TestDocumentTypes.NoAttachments);
+			var pattern = @"\{name:""DummyPattern""";
+
+			// Act
+			var result = PdfTasks.ExtractEkopostRecipientMetadataAndDocumentByRegex(pdfBytes, pattern);
+
+			// Assert
+			Assert.Null(result);
+		}
+
+		[Test]
+		public void ExtractEkopostRecipientMetadataAndDocumentByRegex_SingleMatch_ReturnsOneResult()
+		{
+			// Arrange
+			var pdfBytes = TestHelper.GetTestDocument(TestHelper.TestDocumentTypes.NoAttachments);
+			var pattern = "This is a test document used for unit testing of PDF";
+
+			// Act
+			var result = PdfTasks.ExtractEkopostRecipientMetadataAndDocumentByRegex(pdfBytes, pattern);
+
+			// Assert
+			Assert.NotNull(result);
+
+			var list = result.ToList();
+			Assert.AreEqual(1, list.Count);
+
+			Assert.AreEqual(pattern, list[0].Metadata);
+		}
+
+		[Test]
+		public void ExtractEkopostRecipientMetadataAndDocumentByRegex_MultipleMatches_ReturnsMultipleResults()
+		{
+			// Arrange
+			var pdfBytes = TestHelper.GetTestDocument(TestHelper.TestDocumentTypes.NoAttachments);
+			var pattern = @"(?i)\b(test)\b";
+
+			// Act
+			var result = PdfTasks.ExtractEkopostRecipientMetadataAndDocumentByRegex(pdfBytes, pattern);
+
+			// Assert
+			Assert.NotNull(result);
+			var list = result.ToList();
+			Assert.AreEqual(2, list.Count);
+
+			StringAssert.Contains("test", list[0].Metadata);
+			StringAssert.Contains("Test", list[1].Metadata);
+		}
+
+		[Test]
+		public void ExtractTextByRegex_MultipleMatches_ReturnsMultipleResults()
+		{
+			// Arrange
+			var pdfBytes = TestHelper.GetTestDocument(TestHelper.TestDocumentTypes.NoAttachments);
+			var pattern = "test";
+
+			// Act
+			var result = PdfTasks.ExtractTextByRegex(pdfBytes, pattern);
+
+			// Assert
+			Assert.NotNull(result);
+			StringAssert.Contains("testtest", result);
+		}
+		[Test]
+		public void ExtractTextByRegex_MultipleMatches_ReturnsSingleResults()
+		{
+			// Arrange
+			var pdfBytes = TestHelper.GetTestDocument(TestHelper.TestDocumentTypes.NoAttachments);
+			var pattern = "Watermarks";
+
+			// Act
+			var result = PdfTasks.ExtractTextByRegex(pdfBytes, pattern);
+
+			// Assert
+			Assert.NotNull(result);
+			StringAssert.Contains("Watermarks", result);
+		}
+	}
+}
