cc3e1d6b4a57d99e94eb917df5a175642f339962
patriknilssonk <97089559+patriknilssonk@users.noreply.github.com>
2025-04-28 10:00:34 +0200

    Added new taskmethod MergePdfs

--- Frends.Kungsbacka.Pdf/Frends.Kungsbacka.Pdf.cs
+++ Frends.Kungsbacka.Pdf/Frends.Kungsbacka.Pdf.cs
@@ -1,8 +1,16 @@
 ﻿using Frends.Kungsbacka.Pdf.HtmlToPdf;
 using iText.IO.Source;
 using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Utils;
+using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
 using PdfDocument = iText.Kernel.Pdf.PdfDocument;
 
 namespace Frends.Kungsbacka.Pdf
@@ -270,6 +278,55 @@ namespace Frends.Kungsbacka.Pdf
 				return stringBuilder.ToString();
 			}
 		}
+
+		
+		/// <summary>
+		/// Merges several PDF:s together
+		/// </summary>
+		/// <param name="input">Mandatory parameters</param>
+		/// <returns>PdfDocumentResult {byte[] PdfDocument}</returns>
+		public static PdfDocumentResult MergePdfs([PropertyTab] List<PdfDocumentInput> input)
+		{
+			if (input is null)
+			{
+				throw new ArgumentNullException(nameof(input));
+			}
+
+			if (input.Any(x => x is null))
+			{
+				throw new ArgumentNullException(nameof(input));
+			}
+
+			byte[] result = null;
+
+			using (var outputStream = new MemoryStream())
+			{
+				using (var pdfWriter = new PdfWriter(outputStream))
+				using (var mergedPdf = new PdfDocument(pdfWriter))
+				{
+					var pdfMerger = new PdfMerger(mergedPdf);
+
+					foreach (var pdfBytes in input)
+					{
+						using var tempStream = new MemoryStream(pdfBytes.PdfDocument);
+						using var reader = new PdfReader(tempStream);
+						using var sourcePdf = new PdfDocument(reader);
+
+						pdfMerger.Merge(sourcePdf, 1, sourcePdf.GetNumberOfPages());
+					}
+				}
+
+				result = outputStream.ToArray();
+			}
+
+			var output = new PdfDocumentResult
+			{
+				PdfDocument = result
+			};
+
+			return output;
+		}
+		private class Pdf
         {
             private readonly PdfDocument _document;
             private readonly ByteArrayOutputStream _outputStream;
