6e41e24d3e28b91f1353d45df2ccee8d08c953ec
patriknilssonk <97089559+patriknilssonk@users.noreply.github.com>
2026-05-07 11:10:57 +0200

    Up project-version and .net-version

--- Frends.Kungsbacka.Pdf.Tests/Frends.Kungsbacka.Pdf.Tests.cs
+++ Frends.Kungsbacka.Pdf.Tests/Frends.Kungsbacka.Pdf.Tests.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Frends.Kungsbacka.Pdf.Tests
 {
@@ -574,7 +575,7 @@ namespace Frends.Kungsbacka.Pdf.Tests
 
 			var pdfBytes = TestHelper.GetTestDocument(TestHelper.TestDocumentTypes.ExtractText);
 
-			Assert.Throws<ArgumentException>(() => PdfTasks.ExtractTextByRegex(pdfBytes, regex));
+			Assert.Throws<RegexParseException>(() => PdfTasks.ExtractTextByRegex(pdfBytes, regex));
 		}
 
         [Test]
